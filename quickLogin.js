f5c874a4e665e60a4c988a05242f05455e21c0c7
zhiming <zhiming@ecquaria.com>
2025-11-13 16:32:21 +0800

    load password

--- quickLogin.js
+++ quickLogin.js
@@ -11,6 +11,7 @@
 
 
 (async function () {
+    const DEFAULT_PASSWORD = 'Password$1234'
     // shortcut (ctrl + enter) or (alt + enter) could quickly log in USERS[0]
     // 快捷键(ctrl + enter) 或 (alt + enter) 可以快速登录USERS[0]
     let USERS = []
@@ -29,23 +30,28 @@
         }
         case '192.168.2.11': {
             USERS = [
-                { name: "管理员", accountId: "admin", vault: "0RevY+UTEYTGSHG0vAK/3hjwxmhKes6MFLBZ6LfTDFAptD5gdRdXzGDnZYL7+z5gyB5IYdXln5yUjqoI/KZAFUTgvez0g23uTQGnl0X3aqInG1F9UcckD9Ho2X7igQj6/ZYkPDvq6HtJCT112cG+4xqsJeIHtN10aMM4pOFCCQNlpqvvXCFYDPFMqr98Y61ztNXlDuhdhFEe4/lUsx4UaYpn2nu/qm+Pl5gDiP4vgYkkL65s3uKjFyZMNhat9YJN3aFCmGgaHt6/WhZ/wlufnwL0f9gVWQCvYkIrlXNuzusmrTxLtkSycZVBtpbfYRE71fk6bBpzzyy6UzC+kKiaeg==", /*domainCode: 'optional, default to <designer>'*/ },
+                { name: "管理员", accountId: "admin", password: DEFAULT_PASSWORD, vault: "0RevY+UTEYTGSHG0vAK/3hjwxmhKes6MFLBZ6LfTDFAptD5gdRdXzGDnZYL7+z5gyB5IYdXln5yUjqoI/KZAFUTgvez0g23uTQGnl0X3aqInG1F9UcckD9Ho2X7igQj6/ZYkPDvq6HtJCT112cG+4xqsJeIHtN10aMM4pOFCCQNlpqvvXCFYDPFMqr98Y61ztNXlDuhdhFEe4/lUsx4UaYpn2nu/qm+Pl5gDiP4vgYkkL65s3uKjFyZMNhat9YJN3aFCmGgaHt6/WhZ/wlufnwL0f9gVWQCvYkIrlXNuzusmrTxLtkSycZVBtpbfYRE71fk6bBpzzyy6UzC+kKiaeg==", /*domainCode: 'optional, default to <designer>'*/ },
             ];
             break;
         }
         case '192.168.2.21': {
             USERS = [
-                { name: "管理员", accountId: "admin", vault: "thqVlPGmO8z53Ret3JjKgFcx6o4YfGqoCjSxT51O74bZFgzCtNPeW+VLamA/y9T3mmvFDkOmZHCi038RTp0Wp4ENVMQspd1pX2yZ6/idFx2QgpSyXQ2wMMwXr4i0+ixvOykNSZ/0eWidCRgMWzt6LZiOZFxKsNHk0zY+mw25EqxkkP4JmoklfSlJDr7p4Uqq6WB/Y7cDOtYVZsVh/dI21Gr90B6mgj54Xm2sU7hN0227U9ywLZ9HrDt3z3KxaTAWSbebFVkFU3x9XafLZlHL4ZSfZO7QufgaCkL8AAnKQDcbJUqyiW5vUAdUc4M4MeAXTQwv6lL/14U8Cf1sWMQC1A==" },
+                { name: "管理员", accountId: "admin", password: DEFAULT_PASSWORD, vault: "thqVlPGmO8z53Ret3JjKgFcx6o4YfGqoCjSxT51O74bZFgzCtNPeW+VLamA/y9T3mmvFDkOmZHCi038RTp0Wp4ENVMQspd1pX2yZ6/idFx2QgpSyXQ2wMMwXr4i0+ixvOykNSZ/0eWidCRgMWzt6LZiOZFxKsNHk0zY+mw25EqxkkP4JmoklfSlJDr7p4Uqq6WB/Y7cDOtYVZsVh/dI21Gr90B6mgj54Xm2sU7hN0227U9ywLZ9HrDt3z3KxaTAWSbebFVkFU3x9XafLZlHL4ZSfZO7QufgaCkL8AAnKQDcbJUqyiW5vUAdUc4M4MeAXTQwv6lL/14U8Cf1sWMQC1A==" },
+                // MGF1-sha256
+                { name: "管理员", accountId: "admin", vault: "PAJ4dBcpkeAI7ezBt+zgSfDkEsfoqKEPQFchq87nnH8kpxQi3OmpQwzE+jyIF+yC4Y9M4TNMG8SyD8rNurMe52PKds3VhSgwF0L82ApJ4EMxNBLHG02yzFo1K8sfGJYYPLF5zD70bWBFV+XQClk9A0Ftpgy0EiCnUfPS491PJN7Lz2pW2Ufy/LHN6wSl5f0Lt0DEYqwI9gHePdhP6csToA1A3ezAaP6YMmVG1IqqEp2y3dGVvEqQZfuzPtSCY22GLLeTw0BxMvaWa6sSxK1ariPbvPd5HO86ODzvK4A1sdTh8SBzUIggwdG3sMPWl+O5vjoXpUBOmXfUbPhP5h71zw==" },
             ];
             break;
         }
         default: USERS = [
-            { name: "Zhiming(MGF1_256)", accountId: "zhiming", vault: "MaTvLJGPrZnSzNfhfQpDULHWvdwDTOLD7zg7MZTz9qB8h69twySAIcOrkUwy4ozW58vsYXFSA3eEaOH1deOfUh1zwJHqNMq2409uwGd1aS0zQd7AGMHvwva8mAiZavFN/aF8rV6lcpsjP5aCSKEKIIs4HwVcCK0qBNe8Lb6BZ9PCJEZ7IYbCQNWK9rEgg8jlIVIiThIoi6wUVRMwSrjtnFT+eFqybf8pRUMsdMEo9iLcncyktQv4Umm3HtR+gFikMCTXA/hMSu3wQI/P6a+zx77XGu+n9olwJucpCfrrS1m2AwfoEutRWVVcmD8eZbY7K598Kw0SRzWN0KBYG6H4cg==" },
-            { name: "Zhiming(PKCS1)", accountId: "zhiming", vault: "bn0GDFYvl0hTelADq5RfetjPqxJqffYdHJLXqJv4ZtIiTtpu/9Y44b0soLslkJF5yHVVzpSo6DFYd6JywHXUllvFtaRiCnAc/xyXD1GZfgXC8vKNrbPCclhdAsfyO2y71F2snl9LyNSAOY6wlwcoTNcIPDhYmeS2ZD3ZkeQib6+jfEyIzNJ7X6AMpE+rSEfZPRag3GxgCRH8KuWQaXjrbwwv+x+bw0ft3AYTIfO2QuKSNBcKdHx04gvCj53ecBWdBHMv+IpQuqzN2V0o3AXtwaFcSwYQGUvOwh2MlTzvVmVyZlFP05I6OAU+rcV7KIWCya6S2BQk9C+Hib65R7nTyw==" },
-            { name: "Zhenyu", accountId: "zhenyu", vault: "MaTvLJGPrZnSzNfhfQpDULHWvdwDTOLD7zg7MZTz9qB8h69twySAIcOrkUwy4ozW58vsYXFSA3eEaOH1deOfUh1zwJHqNMq2409uwGd1aS0zQd7AGMHvwva8mAiZavFN/aF8rV6lcpsjP5aCSKEKIIs4HwVcCK0qBNe8Lb6BZ9PCJEZ7IYbCQNWK9rEgg8jlIVIiThIoi6wUVRMwSrjtnFT+eFqybf8pRUMsdMEo9iLcncyktQv4Umm3HtR+gFikMCTXA/hMSu3wQI/P6a+zx77XGu+n9olwJucpCfrrS1m2AwfoEutRWVVcmD8eZbY7K598Kw0SRzWN0KBYG6H4cg==" },
-            { name: "Rongrong", accountId: "rongrong", vault: "Xt6DQb116oMideFa+z87Upv5jhAMR6ChrnCkAG7JDHYMfSu/DGvSHJz6tIvrj7eV0jE3ksGBJPnvp/OtxDbtMuWPJXZHFTCk0jx1s2xF6KDeg8Sm+gFM0tAgUN8/xlHHzDYKHOPdLWNb8fC/bb2lReNUs4mJxzfC9CfHWer9d7AnK71+tpUFgZASHxttIC3NS65D4CGMtPTjaIA+Qu5rQvWEfO1OcwIZpE/1eprlk/XNeMsp1wOs0NqwcuJD/hKvRsGqYK0FzZU/AR1552tP1B6IVYBhh5mGpuyFppvu6M2ckTkdIMsGwB8r3oawCVHTRtFEDm58xiVoSIa5f4fXcQ==" },
-            { name: "Yichun", accountId: "yichun", vault: "bn0GDFYvl0hTelADq5RfetjPqxJqffYdHJLXqJv4ZtIiTtpu/9Y44b0soLslkJF5yHVVzpSo6DFYd6JywHXUllvFtaRiCnAc/xyXD1GZfgXC8vKNrbPCclhdAsfyO2y71F2snl9LyNSAOY6wlwcoTNcIPDhYmeS2ZD3ZkeQib6+jfEyIzNJ7X6AMpE+rSEfZPRag3GxgCRH8KuWQaXjrbwwv+x+bw0ft3AYTIfO2QuKSNBcKdHx04gvCj53ecBWdBHMv+IpQuqzN2V0o3AXtwaFcSwYQGUvOwh2MlTzvVmVyZlFP05I6OAU+rcV7KIWCya6S2BQk9C+Hib65R7nTyw==" },
-            { name: "管理员", accountId: "admin", vault: "ii6yw5jQAviHVP2xxL7g1h0uQjSfjM94iNIQmD1QnhX74BmeNTJ67CKjCq1jMNwfMlK4+prYXsCLnhx5UedUH0PaBZ0fnlXC54J2dgDfmuvNyv1qHZaBHnsrgDAiS4GVNkwncour2lTAwNn4f9vdWfnjvzc8TUcNWHihcEtiEUj7YSyijfNjvjv7A+usH2e65f+zFCDypkIGuo+wwz0+WFKpSSMJGsncwaKIucG1YI+3olR7lvGPVVb2BjfLcLT1eXV5EEP9H/VJxfzUELQSKFZZ0LsuVWwfzvZIZcmkx9oxn0eV5+9zbhma8ItYLxM0j6rU3zYlOc1Q1twmZgGpPQ==" },
+            // { name: "管理员", accountId: "admin", vault: "thqVlPGmO8z53Ret3JjKgFcx6o4YfGqoCjSxT51O74bZFgzCtNPeW+VLamA/y9T3mmvFDkOmZHCi038RTp0Wp4ENVMQspd1pX2yZ6/idFx2QgpSyXQ2wMMwXr4i0+ixvOykNSZ/0eWidCRgMWzt6LZiOZFxKsNHk0zY+mw25EqxkkP4JmoklfSlJDr7p4Uqq6WB/Y7cDOtYVZsVh/dI21Gr90B6mgj54Xm2sU7hN0227U9ywLZ9HrDt3z3KxaTAWSbebFVkFU3x9XafLZlHL4ZSfZO7QufgaCkL8AAnKQDcbJUqyiW5vUAdUc4M4MeAXTQwv6lL/14U8Cf1sWMQC1A==" },
+            { name: "Zhiming(MGF1_256)", accountId: "zhiming", password: DEFAULT_PASSWORD, vault: "MaTvLJGPrZnSzNfhfQpDULHWvdwDTOLD7zg7MZTz9qB8h69twySAIcOrkUwy4ozW58vsYXFSA3eEaOH1deOfUh1zwJHqNMq2409uwGd1aS0zQd7AGMHvwva8mAiZavFN/aF8rV6lcpsjP5aCSKEKIIs4HwVcCK0qBNe8Lb6BZ9PCJEZ7IYbCQNWK9rEgg8jlIVIiThIoi6wUVRMwSrjtnFT+eFqybf8pRUMsdMEo9iLcncyktQv4Umm3HtR+gFikMCTXA/hMSu3wQI/P6a+zx77XGu+n9olwJucpCfrrS1m2AwfoEutRWVVcmD8eZbY7K598Kw0SRzWN0KBYG6H4cg==" },
+            { name: "管理员", accountId: "admin", password: DEFAULT_PASSWORD, vault: "ii6yw5jQAviHVP2xxL7g1h0uQjSfjM94iNIQmD1QnhX74BmeNTJ67CKjCq1jMNwfMlK4+prYXsCLnhx5UedUH0PaBZ0fnlXC54J2dgDfmuvNyv1qHZaBHnsrgDAiS4GVNkwncour2lTAwNn4f9vdWfnjvzc8TUcNWHihcEtiEUj7YSyijfNjvjv7A+usH2e65f+zFCDypkIGuo+wwz0+WFKpSSMJGsncwaKIucG1YI+3olR7lvGPVVb2BjfLcLT1eXV5EEP9H/VJxfzUELQSKFZZ0LsuVWwfzvZIZcmkx9oxn0eV5+9zbhma8ItYLxM0j6rU3zYlOc1Q1twmZgGpPQ==" },
+            // { name: "Zhiming(PKCS1)", accountId: "zhiming", vault: "bn0GDFYvl0hTelADq5RfetjPqxJqffYdHJLXqJv4ZtIiTtpu/9Y44b0soLslkJF5yHVVzpSo6DFYd6JywHXUllvFtaRiCnAc/xyXD1GZfgXC8vKNrbPCclhdAsfyO2y71F2snl9LyNSAOY6wlwcoTNcIPDhYmeS2ZD3ZkeQib6+jfEyIzNJ7X6AMpE+rSEfZPRag3GxgCRH8KuWQaXjrbwwv+x+bw0ft3AYTIfO2QuKSNBcKdHx04gvCj53ecBWdBHMv+IpQuqzN2V0o3AXtwaFcSwYQGUvOwh2MlTzvVmVyZlFP05I6OAU+rcV7KIWCya6S2BQk9C+Hib65R7nTyw==" },
+            // { name: "2.21 server MGF1-sha256管理员", accountId: "admin", vault: "PAJ4dBcpkeAI7ezBt+zgSfDkEsfoqKEPQFchq87nnH8kpxQi3OmpQwzE+jyIF+yC4Y9M4TNMG8SyD8rNurMe52PKds3VhSgwF0L82ApJ4EMxNBLHG02yzFo1K8sfGJYYPLF5zD70bWBFV+XQClk9A0Ftpgy0EiCnUfPS491PJN7Lz2pW2Ufy/LHN6wSl5f0Lt0DEYqwI9gHePdhP6csToA1A3ezAaP6YMmVG1IqqEp2y3dGVvEqQZfuzPtSCY22GLLeTw0BxMvaWa6sSxK1ariPbvPd5HO86ODzvK4A1sdTh8SBzUIggwdG3sMPWl+O5vjoXpUBOmXfUbPhP5h71zw==" },
+            { name: "Zhenyu", accountId: "zhenyu", password: DEFAULT_PASSWORD, vault: "MaTvLJGPrZnSzNfhfQpDULHWvdwDTOLD7zg7MZTz9qB8h69twySAIcOrkUwy4ozW58vsYXFSA3eEaOH1deOfUh1zwJHqNMq2409uwGd1aS0zQd7AGMHvwva8mAiZavFN/aF8rV6lcpsjP5aCSKEKIIs4HwVcCK0qBNe8Lb6BZ9PCJEZ7IYbCQNWK9rEgg8jlIVIiThIoi6wUVRMwSrjtnFT+eFqybf8pRUMsdMEo9iLcncyktQv4Umm3HtR+gFikMCTXA/hMSu3wQI/P6a+zx77XGu+n9olwJucpCfrrS1m2AwfoEutRWVVcmD8eZbY7K598Kw0SRzWN0KBYG6H4cg==" },
+            { name: "Rongrong", accountId: "rongrong", password: DEFAULT_PASSWORD, vault: "Xt6DQb116oMideFa+z87Upv5jhAMR6ChrnCkAG7JDHYMfSu/DGvSHJz6tIvrj7eV0jE3ksGBJPnvp/OtxDbtMuWPJXZHFTCk0jx1s2xF6KDeg8Sm+gFM0tAgUN8/xlHHzDYKHOPdLWNb8fC/bb2lReNUs4mJxzfC9CfHWer9d7AnK71+tpUFgZASHxttIC3NS65D4CGMtPTjaIA+Qu5rQvWEfO1OcwIZpE/1eprlk/XNeMsp1wOs0NqwcuJD/hKvRsGqYK0FzZU/AR1552tP1B6IVYBhh5mGpuyFppvu6M2ckTkdIMsGwB8r3oawCVHTRtFEDm58xiVoSIa5f4fXcQ==" },
+            { name: "Yichun", accountId: "yichun", password: DEFAULT_PASSWORD, vault: "bn0GDFYvl0hTelADq5RfetjPqxJqffYdHJLXqJv4ZtIiTtpu/9Y44b0soLslkJF5yHVVzpSo6DFYd6JywHXUllvFtaRiCnAc/xyXD1GZfgXC8vKNrbPCclhdAsfyO2y71F2snl9LyNSAOY6wlwcoTNcIPDhYmeS2ZD3ZkeQib6+jfEyIzNJ7X6AMpE+rSEfZPRag3GxgCRH8KuWQaXjrbwwv+x+bw0ft3AYTIfO2QuKSNBcKdHx04gvCj53ecBWdBHMv+IpQuqzN2V0o3AXtwaFcSwYQGUvOwh2MlTzvVmVyZlFP05I6OAU+rcV7KIWCya6S2BQk9C+Hib65R7nTyw==" },
+            { name: "管理员", accountId: "admin", password: DEFAULT_PASSWORD, vault: "ii6yw5jQAviHVP2xxL7g1h0uQjSfjM94iNIQmD1QnhX74BmeNTJ67CKjCq1jMNwfMlK4+prYXsCLnhx5UedUH0PaBZ0fnlXC54J2dgDfmuvNyv1qHZaBHnsrgDAiS4GVNkwncour2lTAwNn4f9vdWfnjvzc8TUcNWHihcEtiEUj7YSyijfNjvjv7A+usH2e65f+zFCDypkIGuo+wwz0+WFKpSSMJGsncwaKIucG1YI+3olR7lvGPVVb2BjfLcLT1eXV5EEP9H/VJxfzUELQSKFZZ0LsuVWwfzvZIZcmkx9oxn0eV5+9zbhma8ItYLxM0j6rU3zYlOc1Q1twmZgGpPQ==" },
             // 可添加更多用户
         ]
     }
@@ -88,12 +94,30 @@
 
         return remove;
     }
-    let prefixCache;
-    function getCsrfTokenPrefix() {
-        if(prefixCache) return prefixCache;
-        const defaultPrefix = 'agp-cookie-csrf';
 
-        return fetch('/config.json')
+    let publicKeyCache
+    async function loadPublicKey() {
+        if (publicKeyCache) return publicKeyCache
+        return await fetch('/public/iam/api/v1/.wellKnown/publicKey')
+            .then(async response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json()
+                console.log('resp>>>', data)
+                publicKeyCache = JSON.parse(data?.data)
+                console.log('publicKeyCache', publicKeyCache)
+                return JSON.parse(data?.data)
+            }).catch(error => {
+                console.error('Failed to fetch public key:', error);
+            });
+    }
+
+
+    let siteConfig
+    async function loadSiteConfig() {
+        if(siteConfig) return siteConfig;
+        await fetch('/config.json')
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
@@ -101,19 +125,71 @@
                 return response.json();
             })
             .then(config => {
-                prefixCache = config.CSRF_TOKEN_PREFIX || defaultPrefix;
-                return config.CSRF_TOKEN_PREFIX || defaultPrefix;
+                siteConfig = config
+                return config;
             })
             .catch(error => {
-                prefixCache = defaultPrefix;
                 console.error('Failed to fetch config.json:', error);
-                return defaultPrefix;
+                return siteConfig;
             });
+    }
+
+    async function getCsrfTokenPrefix() {
+        const config = await loadSiteConfig();
+        const defaultPrefix = 'agp-cookie-csrf';
+        return config?.CSRF_TOKEN_PREFIX || defaultPrefix;
+    }
+
+
+    // 将 JWK 格式导入为 CryptoKey
+    async function importPublicKey(jwk, padding = 'MGF1_SHA256') {
+        const alg = {
+            name: padding.startsWith('PKCS1') ? 'RSAES-PKCS1-v1_5' : 'RSA-OAEP',
+            hash: padding.endsWith('SHA1') ? 'SHA-1' : 'SHA-256',
+        };
+
+        return await crypto.subtle.importKey(
+            'jwk',
+            jwk,
+            alg,
+            false, // 不可导出
+            ['encrypt'],
+        );
+    }
+
+// 加密函数
+    async function encryptByPadding(encryptText, jwk, padding = 'MGF1_SHA256') {
+        const key = await importPublicKey(jwk, padding);
+        const enc = new TextEncoder().encode(encryptText);
+
+        const algo =
+            padding.startsWith('PKCS1')
+                ? { name: 'RSAES-PKCS1-v1_5' }
+                : { name: 'RSA-OAEP' };
+
+        const encrypted = await crypto.subtle.encrypt(algo, key, enc);
+        return btoa(String.fromCharCode(...new Uint8Array(encrypted)));
+    }
+
+// 对外暴露主函数
+    async function encrypt(encryptText, jwk) {
+        const config = await loadSiteConfig()
+        const padding = (config?.['RSA_ENCRYPTION_PADDING'] ?? 'MGF1_SHA256').toUpperCase();
+
+        if (padding === 'MGF1' || padding === 'MGF1_SHA1') {
+            return await encryptByPadding(encryptText, jwk, 'MGF1_SHA1');
+        } else if (padding === 'PKCS1') {
+            return await encryptByPadding(encryptText, jwk, 'PKCS1');
+        } else {
+            // 默认 MGF1 + SHA256
+            return await encryptByPadding(encryptText, jwk, 'MGF1_SHA256');
+        }
     }
 
     async function getCSRF() {
         const cookies = document.cookie.split('; ');
         const csrfKey = await getCsrfTokenPrefix()
+        loadSiteConfig('csrfKey>>', csrfKey)
         for (const cookie of cookies) {
             const [key, value] = cookie.split('=');
             if (key === csrfKey) {
@@ -127,12 +203,17 @@
     async function loginWithUser(user) {
         const hide = showToast('loading', '正在登录 ' + user.name);
         try {
+            const pubKey = await loadPublicKey()
+            let vault = user.vault
+            if (pubKey && user.password) {
+                vault = await encrypt(user.password, pubKey)
+            }
             const res = await fetch('/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json', 'X-XSRF-TOKEN': await getCSRF() },
                 body: JSON.stringify({
                     userAccountId: user.accountId,
-                    vault: user.vault,
+                    vault,
                     userDomainCode: user.domainCode ?? 'designer'
                 })
             });
@@ -218,7 +299,7 @@
 
 
         // 加载显示/隐藏状态
-        const visible = localStorage.getItem('login-fabVisible');
+        const visible = localStorage.getItem('login-fabVisible') ?? 'false';
         if (visible === 'false') {
             wrapper.style.display = 'none';
         } else {
